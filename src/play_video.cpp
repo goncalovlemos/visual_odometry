59fe8ecbcb94e57bfb86df933d4656e76e541399
andrefpreto <andrefpreto@gmail.com>
2019-10-04 22:26:29 +0100

    Delivered version

--- src/play_video.cpp
+++ src/play_video.cpp
@@ -41,60 +41,14 @@ cv::Mat imaged, previmaged;
 
 vector<Point2f> corners, prevcorners;
 
-std::string rosbagstring = "/camera_meio/led/compressed"; 
-std::string nodename = "/camera_meio"; 
+std::string rosbagstring = "/camera_meio/led/compressed";
+std::string nodename = "/camera_meio";
 
 vector<Point2f> cornersf, prevcornersf;
 vector<Point2f> cornerse, prevcornerse;
 vector<Point2f> cornersd, prevcornersd;
 
 int ofcnt = 0;
-
-//----------------------- EXTRA FUNCTIONS HERE
-/*
-void epipolarlines(Mat leftImage, Mat rightImage, vector<Point2f> imagePointsLeftCamera, vector<Point2f> imagePointsRightCamera){
-	cv::Mat fundamentalMatrix= cv::findFundamentalMat(imagePointsLeftCamera, imagePointsRightCamera, FM_8POINT);
-	std::vector<cv::Vec3d> leftLines, rightLines;
-	cv::computeCorrespondEpilines(imagePointsLeftCamera, 1, fundamentalMatrix, rightLines);
-    cv::computeCorrespondEpilines(imagePointsRightCamera, 2, fundamentalMatrix, leftLines);
-	
-    cv::Mat leftImageRGB(leftImage.size(), CV_8UC3);
-    cv::cvtColor(leftImage, leftImageRGB, CV_GRAY2RGB);
-	
-    cv::Mat rightImageRGB(rightImage.size(), CV_8UC3);
-    cv::cvtColor(rightImage, rightImageRGB, CV_GRAY2RGB);
- 
-    cv::Mat imagePointLeftCameraMatrix=cv::Mat_<double>(3,1);
- 
-    for(std::size_t i=0;i<rightLines.size();i=i+1)
-    {
-        cv::Vec3d l=rightLines.at(i);
-	double a=l.val[0];
-        double b=l.val[1];
-        double c=l.val[2];
-        std::cout<<"------------------------a,b,c Using OpenCV (ax+by+c=0)------------------------------"<<std::endl;
-        std::cout<< a <<", "<<b <<", "<<c <<std::endl;
-        std::cout<<"------------------------calculating a,b,c (ax+by+c=0) ------------------------------"<<std::endl;
-        
-        imagePointLeftCameraMatrix.at<double>(0,0)=imagePointsLeftCamera[i].x;
-        imagePointLeftCameraMatrix.at<double>(1,0)=imagePointsLeftCamera[i].y;
-        imagePointLeftCameraMatrix.at<double>(2,0)=1;
-        cv::Mat rightLineMatrix=fundamentalMatrix*imagePointLeftCameraMatrix;
-        
-        std::cout<< rightLineMatrix.at<double>(0,0) <<", "<<rightLineMatrix.at<double>(0,1) <<", "<<rightLineMatrix.at<double>(0,2) <<std::endl;
- 
-        //drawing the line on the image
-        //ax+by+c=0
-        double x0,y0,x1,y1;
-        x0=0;
-        y0=(-c-a*x0)/b;
-	x1=rightImageRGB.cols;
-        y1=(-c-a*x1)/b;
- 
-	std::cout<<"error: "<< a*imagePointsRightCamera.at(i).x+ b*imagePointsRightCamera.at(i).y +c<<std::endl;
-	cv::line(rightImageRGB, cvPoint(x0,y0), cvPoint(x1,y1), cvScalar(0,255,0), 1);
-    }
-}*/
 
 namespace patch //std::to_string patch: there's a known bug with to_string not being a member of std when compiling
 {
@@ -108,15 +62,15 @@ namespace patch //std::to_string patch: there's a known bug with to_string not b
 
 Mat correctGamma( Mat& img, double gamma ) {
  double inverse_gamma = 1.0 / gamma;
- 
+
  Mat lut_matrix(1, 256, CV_8UC1 );
  uchar * ptr = lut_matrix.ptr();
  for( int i = 0; i < 256; i++ )
    ptr[i] = (int)( pow( (double) i / 255.0, inverse_gamma ) * 255.0 );
- 
+
  Mat result;
  LUT( img, lut_matrix, result );
- 
+
  return result;
 }
 
@@ -125,7 +79,7 @@ int cntr = 0;
 void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg)
 {
   try
-  { 
+  {
 	const char *cstr = rosbagstring.c_str();
 	if(strcmp(cstr,"/camera_meio/led/compressed")==0){
 		std::swap(corners, cornersf);
@@ -159,13 +113,13 @@ void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg)
 	}
 
 	//image = 0.25* (image + 200);
-	image = correctGamma(image, 0.9);	
+	image = correctGamma(image, 0.9);
 
 	Mat img;
 	cv::resize(image, img, cv::Size(), 0.25, 0.25);
 	//if(SHOW_VIDEO_FEED == 1)cv::imshow("view" , img);
 	cv::waitKey(10);
-	
+
 //----------------------- Detector parameters (Shi-Tomasi corner detector)
 
 	double qualityLevel = 0.03;
@@ -220,10 +174,11 @@ void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg)
 
 	calcOpticalFlowPyrLK(previmage, image, prevcorners, corners, status, err, winSize, 3, termcrit, 0, 0.001);
 
+
 //----------------------- Draw features
 
-ndetected = 0;
-	for( int i = 0; i < corners.size() ; i++ ){ 
+
+	for( int i = 0; i < ndetected; i++ ){
 	if(status[i] == 1){
 		circle( image_copy, Point( corners[i].x, corners[i].y ), 5,  Scalar(0), 2, 8, 0 );
 		if(PRINT_FEATURES_LOCATION == 1){
@@ -233,7 +188,7 @@ ndetected = 0;
 		ndetected = ndetected + 1;
 
 	}
-	}	
+	}
 	cv::resize(image_copy, img, cv::Size(), 0.50, 0.50);
 	if(SHOW_VIDEO_FEED == 1)cv::imshow("corners" , img);
 
@@ -264,13 +219,6 @@ ndetected = 0;
 
         }
 
-/*
-	Mat imgof;
-        cv::add(frame, mask, imgof);
-	cv::resize(image_copy, imgof, cv::Size(), 0.50, 0.50);
-        if(SHOW_FLOW == 1)imshow("Optical Flow", mask);
-*/
-	
 
 	if (ofcnt == 100){
 		mask = Mat::zeros(image.size(), image.type());
@@ -295,52 +243,8 @@ ndetected = 0;
 		cv::add(imgof, img, imgof);
 		imshow("Optical Flow", imgof);
 
-//		cv::resize(mask, mask, cv::Size(), 0.50, 0.50);
-//		cv::add(imgof,mask,imgof);
-		imshow("Optical Flow", imgof);
-	}
-	
-//----------------------- Epipolar Check
-
-	//Mat rigidtransform = estimateRigidTransform(prevcorners, corners, 0);
-	//epipolarlines(previmage, image, prevcorners, corners);
-	//cv::Mat fundM = cv::findFundamentalMat(prevcorners, corners, FM_8POINT);
-
-	//std::vector<cv::Vec3d> prevlines, lines;
-
-
-
-//----------------------- PUBLISH DATA
-/*
-	std_msgs::Float32MultiArray dat;
-
-	dat.layout.dim.push_back(std_msgs::MultiArrayDimension());
-	dat.layout.dim.push_back(std_msgs::MultiArrayDimension());
-	dat.layout.dim[0].label = "height";
-	dat.layout.dim[1].label = "width";
-	int H = prevcorners.size();
-	dat.layout.dim[0].size = H;
-	int W = 2;
-	dat.layout.dim[1].size = W;
-	dat.layout.dim[0].stride = H*W;
-	dat.layout.dim[1].stride = W;
-	dat.layout.data_offset = 0;
-
-	ros::NodeHandle nh;
-	ros::Publisher pub = nh.advertise<std_msgs::Float32MultiArray>("uwv_2d_data", 1000);
-
-		dat.data.push_back(ndetected);
-	for (int i=0; i<H; i++){
-		dat.data.push_back(prevcorners[i].x);
-		dat.data.push_back(prevcorners[i].y);
 	}
 
-	pub.publish(dat);
-	dat.data.clear();
-
-
-	//printf("\n\n\n\n %d \n\n\n\n ",ndetected); //debug
-*/
 //----------------------- GET POSE
 
 	vector<Point2f> good_corners;
@@ -371,35 +275,18 @@ ndetected = 0;
 	E = findEssentialMat(corners, prevcorners, focal, pp, RANSAC, 0.999, 1.0, mask);
 	recoverPose(E, corners, prevcorners, R, t, focal, pp, mask);
 
+  cout << endl << "--------------------------------------------" << endl;
+  cout << "Features Detected: " << ndetected << endl << endl;
+  cout << "Features Matched: " << corners.size() << endl << endl;
+	cout << "Rotation Matrix" << endl << R << endl << endl;
+  cout << "Translation Vector" << endl << t << endl << endl;
 
 //----------------------- Prepare new cycle
 
+  ndetected = 0;
 	std::swap(corners, prevcorners);
 	cv::swap(previmage, image);
 	destroyAllWindows;
-
-//-------------------------------------------------------------------------- PRINT FOR TXT LOGS
-//-------------------------------------------------------------------------- PRINT FOR TXT LOGS
-//-------------------------------------------------------------------------- PRINT FOR TXT LOGS
-
-	struct timeval tp;
-	gettimeofday(&tp, NULL);
-	long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-
-	cout << ms << " ";
-
-	double* mtemp1 = &R.at<double>(0);
-	for(int i=0;i<9;i++)cout << mtemp1[i] << " ";
-
-	double* mtemp2 = &t.at<double>(0);
-	for(int i=0;i<3;i++)cout << mtemp2[i] << " ";
-
-	cout << endl;
-/*
-	cout << ms << endl;
-	cout << R << endl;
-	cout << t << endl;
-*/
 
 //----------------------- PUBLISH R&t DATA
 
@@ -456,7 +343,7 @@ void fhelp(void){
 	printf("\n-pfloc            : print feature location in video feed");
 	printf("\n-vfeed            : enable video feed");
 	printf("\n-oflow            : enable video feed with optical flow");
-	printf("\n-save             : save frames in a folder");	
+	printf("\n-save             : save frames in a folder");
 	printf("\n---------------------------------------------\n\n");
 	return;
 }
@@ -466,11 +353,11 @@ void fhelp(void){
 
 int main(int argc, char **argv)
 {
- 
+
   if(argc > 0){
 	for(int i=0; i < argc; i++){
 		if (strcmp(argv[i], "-bag") == 0){
-			rosbagstring = argv[i+1];			
+			rosbagstring = argv[i+1];
 			cout << "STRING:" << endl << rosbagstring << endl << endl;
 		}
 		if (strcmp(argv[i], "-maxfcnt") == 0)MAX_FEATURE_COUNT = atoi(argv[i+1]);
@@ -490,9 +377,9 @@ if((rosbagstring.empty()) && (helppage = 0)){
 */
 
 	const char *cstr = rosbagstring.c_str();
-	if(strcmp(cstr,"/camera_meio/led/compressed")==0)nodename = "camera_meio"; 
-	if(strcmp(cstr,"/camera_direita/led/compressed")==0)nodename = "camera_direita"; 
-	if(strcmp(cstr,"/camera_esquerda/led/compressed")==0)nodename = "camera_esquerda"; 
+	if(strcmp(cstr,"/camera_meio/led/compressed")==0)nodename = "camera_meio";
+	if(strcmp(cstr,"/camera_direita/led/compressed")==0)nodename = "camera_direita";
+	if(strcmp(cstr,"/camera_esquerda/led/compressed")==0)nodename = "camera_esquerda";
 
   ros::init(argc, argv, nodename);
   ros::NodeHandle nh;
@@ -501,11 +388,9 @@ if((rosbagstring.empty()) && (helppage = 0)){
   ros::Subscriber sub = nh.subscribe(rosbagstring, 1, imageCallback);
   ros::Publisher pub = nh.advertise<std_msgs::Float32MultiArray>("uwv_rt_data", 1000);
   //ros::Publisher pub = nh.advertise<std_msgs::Float32MultiArray>("uwv_2d_data", 1000);
-  
+
 
   printf("\n\nWaiting for video input with '/camera_meio/led/compressed' topic...\n");
   ros::spin();
   //cv::destroyWindow("view");
 }
-
-
